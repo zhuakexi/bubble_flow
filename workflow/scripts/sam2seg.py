4b0959c889d50da3f10b42f91ef46aad03af46eb
zhuakexi <zhuakexi@126.com>
2022-09-07 15:38:46 +0800

    simplified sam2seg scripts, rely on python

--- workflow/scripts/sam2seg.py
+++ workflow/scripts/sam2seg.py
@@ -1,117 +1,77 @@
 from sqlite3 import paramstyle
 from subprocess import check_output
-def sam2segW(cfg, input_, params, output, log):
+def sam2segW(cfg, sam, mode_deduction, params, output, log):
     """
     Input:
-        input_: must have
-            sam: sam file path, str
-            sex: `X`,`Y`,`XY`,`XX`, str
-            snp: `using` for using, str
+        cfg: softwares path
+        mode_deduction: mode str deducted from sample_checking, must be [ploidy]_[sex]_[snp], eg:
+            2C_lY_
+        params: per sample mode assignment from config or sample_table, same as mode_deduction.
+        output: output file path
+        log: log file path
+    Note:
+        ploidy: `2C` `1C` ``
+        sex: `lY` `hY` ``
+        snp: `NO` `SNP`
+        deduction SNP is always ``, ploidy and sex can't be ``.
     """
-    segstr = params.split("_")
-    #if params["sex"] == "X" and params["snp"]=="using":
-    if ("X" in segstr) and ("SNP" in segstr):
-        # haploid and X, using snp
-        # add snp phase col, remove chrY reads
-        check_output(
-            """{k8} {js} sam2seg -v {snp} {input_} 2> {log} \
-            | {k8} {js} chronly -y - \
+    # params assignment first, if missing, using deduction
+    ploidy, sex, snp = [ e if e != "" else mode_deduction.split("_")[i] for i, e in enumerate(params.split("_")) ]
+    code =  """{{k8}} {{js}} {sam2seg} 2> {{log}} \
+            | {{k8}} {{js}} {chrfilt} - \
+            {bedfilt} \
             | sed 's/-/+/g' \
-            | gzip > {output}\
-            """.format(
-                k8 = cfg["software"]["k8"],
-                js = cfg["software"]["js"],
-                snp = cfg["reference"]["snp"],
-                input_ = input_,
-                output = output,
-                log = log
-            ),
-            shell=True)
-    #elif params["sex"] == "Y" and params["snp"]=="using":
-
-    elif ("Y" in segstr) and ("SNP" in segstr):
-        # haploid and Y, using snp
-        # add snp phase col, remove chrX reads
-        # note: gamete has Y chromosome only
-        #   must remove X chromosome, otherwise 3d build defects
-        check_output(
-            """{k8} {js} sam2seg -v {snp} {input_} 2> {log} \
-            | {k8} {js} chronly -r '^(chr)?([0-9]+|[Y])$' - \
-            | {k8} {js} bedflt {PAR} - \
-            | sed 's/-/+/g' \
-            | gzip > {output}\
-            """.format(
-                k8 = cfg["software"]["k8"],
-                js = cfg["software"]["js"],
-                snp = cfg["reference"]["snp"],
-                PAR = cfg["reference"]["par"],
-                input_ = input_,
-                output = output,
-                log = log
-            ),
-            shell=True)
-    #elif params["sex"] == "X" and params["snp"]!="using":
-    elif ( ("X" in segstr) or ("XX" in segstr) ) and ("SNP" not in segstr):
-        # 1. haploid and X, using snp
-        # 2. diploid and XX, using snp
-        # add snp phase col, remove chrY reads
-        check_output(
-            """{k8} {js} sam2seg {input_} 2> {log} \
-            | {k8} {js} chronly -y - \
-            | sed 's/-/+/g' \
-            | gzip > {output}\
-            """.format(
-                k8 = cfg["software"]["k8"],
-                js = cfg["software"]["js"],
-                input_ = input_,
-                output = output,
-                log = log
-            ),
-            shell=True)
-    #elif params["sex"] == "Y" and params["snp"]!="using":
-    elif ("Y" in segstr) and ("SNP" not in segstr):
-        # haploid and Y, using snp
-        # add snp phase col, remove chrX reads
-        # note: gamete has Y chromosome only
-        #   must remove X chromosome, otherwise 3d build defects
-        check_output(
-            """{k8} {js} sam2seg {input_} 2> {log} \
-            | {k8} {js} chronly -r '^(chr)?([0-9]+|[Y])$' - \
-            | {k8} {js} bedflt {PAR} - \
-            | sed 's/-/+/g' \
-            | gzip > {output}\
-            """.format(
-                k8 = cfg["software"]["k8"],
-                js = cfg["software"]["js"],
-                PAR = cfg["reference"]["par"],
-                input_ = input_,
-                output = output,
-                log = log
-            ),
-            shell=True)
-    elif ("XY" in segstr) and ("SNP" in segstr):
-        # diploid and XY, using snp
-        # add snp phase col, remove XY pseudoautosome reads
-        check_output(
-            """{k8} {js} sam2seg {input_} 2> {log} \
-            | {k8} {js} chronly - \
-            | {k8} {js} bedflt {PAR} - \
-            | sed 's/-/+/g' \
-            | gzip > {output}\
-            """.format(
-                k8 = cfg["software"]["k8"],
-                js = cfg["software"]["js"],
-                PAR = cfg["reference"]["par"],
-                input_ = input_,
-                output = output,
-                log = log
-            ),
-            shell=True)
+            | gzip > {{output}}\
+            """
+    if snp == "SNP":
+        # add snp col in seg and pairs
+        sam2seg = "sam2seg -v {snp} {input_}"
     else:
-        raise ValueError("sam2segW: wrong value in sex or snp col in sample_table.csv")
+        sam2seg = "sam2seg {input_}"
+    if ploidy == "1C":
+        if sex == "hY":
+            # high Y
+            # remove X reads, 
+            # filter out pseudoautosome region (don't know why, copy from my mouse sperm code")
+            # note: gamete has Y chromosome only
+            #   must remove X chromosome, otherwise 3d build defects
+            chrfilt = "chronly -r '^(chr)?([0-9]+|[Y])$'"
+            bedfilt = "| {k8} {js} bedflt {PAR} - "
+        elif sex == "lY":
+            # low Y
+            # remove Y reads
+            chrfilt = "chronly -y"
+            bedfilt = ""
+    elif ploidy == "2C":
+        if sex == "hY":
+            # remove pseudoautosome region
+            chrfilt = "chronly"
+            bedfilt = "| {k8} {js} bedflt {PAR} - "
+        elif sex == "lY":
+            # remove Y chr reads
+            chrfilt = "chronly -y"
+            bedfilt = ""
+    else:
+        raise ValueError("sam2segW: wrong sex or ploidy in params mode or mode_deduction")
+    code = code.format(
+        sam2seg=sam2seg,
+        chrfilt=chrfilt,
+        bedfilt = bedfilt)
+    print(code)
+    code = code.format(
+            k8 = cfg["software"]["k8"],
+            js = cfg["software"]["js"],
+            snp = cfg["reference"]["snp"],
+            PAR = cfg["reference"]["par"],
+            input_ = sam,
+            output = output,
+            log = log
+    )
+    return code
 sam2segW(
     snakemake.config,
-    snakemake.input,
+    snakemake.input.get("sam"),
+    snakemake.input.get("mode_deduction"),
     snakemake.params[0],
     snakemake.output,
     snakemake.log
