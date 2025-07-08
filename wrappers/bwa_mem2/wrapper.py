bd734123250b8284ab23f37e6cac2f986939aa6f
zhuakexi <zhuakexi@126.com>
2025-07-08 15:40:07 +0800

    add tmp-prefix for samtools

--- wrappers/bwa_mem2/wrapper.py
+++ wrappers/bwa_mem2/wrapper.py
@@ -3,17 +3,22 @@ __author__ = "Y Chi"
 __copyright__ = "Copyright 2020, Y Chi"
 __email__ = "zhuakexi@126.com"
 __license__ = "MIT"
+from pathlib import Path
 from snakemake.shell import shell
 import os
 extra = snakemake.params.get("extra")
 threads = snakemake.threads
 #index = snakemake.params.index
+sample_name = snakemake.wildcards.sample
 index = snakemake.input.index
 input = snakemake.input
 log = snakemake.log
 output = snakemake.output
 bwa_threads = snakemake.config["cpu"]["bwa"]
 samsort_threads = snakemake.config["cpu"]["samsort"]
+tmp_dir = Path(snakemake.config["ana_home"]) / "tmp"
+# Check if tmp_dir exists, if not create it
+Path(tmp_dir).mkdir(parents=True, exist_ok=True)
 
 shell(
     " bwa-mem2 mem "
@@ -22,6 +27,6 @@ shell(
     " -t {bwa_threads} "
     " {index} "
     " {input.R1} {input.R2} " 
-    " | samtools sort -@{samsort_threads} -o {output} -O BAM - ; "
+    " | samtools sort -@{samsort_threads} -o {output} -T {tmp_dir}/{sample_name}_bwa -O BAM - ; "
     " samtools index -@ {threads} {output} "
 )
